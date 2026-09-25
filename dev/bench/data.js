8661daf0f96ae4c602300390ac7a04ab5e49f141
github-action-benchmark <github@users.noreply.github.com>
2026-09-25 17:09:30 +0000

    add Lizard erosion (customSmallerIsBetter) benchmark result for c854b62f03fa152fe46145747268cad2f7f99ed1

---
+++ dev/bench/data.js
@@ -0,0 +1,57 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1790356170596,
+  "repoUrl": "https://github.com/leugenea/qmix",
+  "entries": {
+    "Lizard erosion": [
+      {
+        "commit": {
+          "author": {
+            "email": "leugenea@gmail.com",
+            "name": "Luckyanets Eugene",
+            "username": "leugenea"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "c854b62f03fa152fe46145747268cad2f7f99ed1",
+          "message": "ci: record erosion history on main and publish the chart (#227)\n\nRefs #198.",
+          "timestamp": "2026-09-25T20:08:54+03:00",
+          "tree_id": "5806382fa1026fe056e31878bb7cd53947799b08",
+          "url": "https://github.com/leugenea/qmix/commit/c854b62f03fa152fe46145747268cad2f7f99ed1"
+        },
+        "date": 1790356169510,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "Overall erosion",
+            "value": 34.18758948687785,
+            "unit": "%"
+          },
+          {
+            "name": "Go erosion",
+            "value": 35.66333549473505,
+            "unit": "%"
+          },
+          {
+            "name": "Kotlin erosion",
+            "value": 32.96752101886259,
+            "unit": "%"
+          },
+          {
+            "name": "JS erosion",
+            "value": 0,
+            "unit": "%"
+          },
+          {
+            "name": "Functions with CCN > 10",
+            "value": 23,
+            "unit": "functions"
+          }
+        ]
+      }
+    ]
+  }
+}
